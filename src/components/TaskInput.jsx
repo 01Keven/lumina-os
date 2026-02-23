689d7f47bb444a8a108a9897549d64c5562f8ac3
01Keven <kevenchristiancode@gmail.com>
2026-02-23 18:14:13 -0300

    refactor TaskInput component to validate input length before adding task

--- src/components/TaskInput.jsx
+++ src/components/TaskInput.jsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 
-export function TaskInput(onAdd) {
+export function TaskInput({onAdd}) {
     const [text, setText] = useState('');
+    const handleAction = () => {
+        if (text.trim().length >= 3) {
+            onAdd(text)
+            setText('')
+        } else {
+            alert('A tarefa precisa de pelo menos 3 caracteres')
+        }
+    }
 
     return (
         <div>
             <input value={text}
             onChange={e => setText(e.target.value)} />
-            <button onClick={onAdd}> Add
+            <button onClick={handleAction}> Add
             </button>
         </div>
     )
